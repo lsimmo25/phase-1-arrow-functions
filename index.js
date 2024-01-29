89b0033525f15d466e91273592eae61eda5a6beb
Lyle Simmons <lylesimmons125@gmail.com>
2024-01-29 18:16:34 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (a, b) => {
+    //let a = 2000;
+    //let b = 100;
+    return a / b;
+}
+
+const square = (x) => {
+    //let x = 2;
+    return x ** 2;
+}
+
+const add = (a, b) => {
+    //let a = 3;
+    //let b = 4;
+    return a + b;
+}
